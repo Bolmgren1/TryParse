be54e47417c7746b4c4f1a1b90c0a7bf86e90e13
Bolmgren1 <kebo05001@utb.vaxjo.se>
2024-02-01 11:06:17 +0100

    .

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        int resultat;
+        if (!TryParse(Console.ReadLine(), out resultat))
+            Console.WriteLine("Skriv in en siffra");
+
+        static bool TryParse(string s, out int resultat)
+        {
+            try
+            {
+                resultat = int.Parse(s);
+                return true;
+            }
+
+            catch
+            {
+                resultat = -1;
+                return false;
+            }
+        }
+    }
+}
